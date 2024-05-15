12dd2711a34496c6f20679d3f1a576236dd067cf
wallacemiller <wallacemillerdias@gmail.com>
2024-05-15 09:36:13 -0300

    criando o projeto para exemplo de tipos variaveis

--- tipos-vairaveis/src/TiposVariaveis.java
+++ tipos-vairaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        byte numero1 = 100;
+        short  numero2=numero1;
+        numero1 = (byte) numero2;
+        System.out.println(numero1);
+      }
 }
